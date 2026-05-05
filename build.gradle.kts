7546e1f22d49a153f28d964e5c1a099810d8b921
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-05 13:24:48 -0700

    Remove deprecated Apple x64 targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,21 +43,9 @@ kotlin {
             xcf.add(this)
         }
     }
-    macosX64 {
-        binaries.framework {
-            baseName = "Anstyle"
-            xcf.add(this)
-        }
-    }
     linuxX64()
     mingwX64()
     iosArm64 {
-        binaries.framework {
-            baseName = "Anstyle"
-            xcf.add(this)
-        }
-    }
-    iosX64 {
         binaries.framework {
             baseName = "Anstyle"
             xcf.add(this)
@@ -132,7 +120,7 @@ val enableIosSimulatorTests =
     providers.gradleProperty("enableIosSimulatorTests").map { it.toBoolean() }.orElse(false)
 
 tasks.withType<KotlinNativeTest>().configureEach {
-    if (!enableIosSimulatorTests.get() && (name == "iosX64Test" || name == "iosSimulatorArm64Test")) {
+    if (!enableIosSimulatorTests.get() && name == "iosSimulatorArm64Test") {
         enabled = false
     }
 }
